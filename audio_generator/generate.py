4f498a548685b7ee855e97fca84fb8f084b3664a
Valentyn <fedetsov@gmail.com>
2024-09-18 18:27:59 +0700

    Audio generator has been created

---
+++ audio_generator/generate.py
@@ -0,0 +1,20 @@
+import os
+from datetime import datetime
+from openai import OpenAI
+open_key = os.environ.get('OPENAI_API_KEY')
+client = OpenAI(api_key=open_key)
+answer = """Hello, I am Global Predictor AI. Curious to know what insights I have for you today?
+"""
+string_to_replace = ['-',':',' ','.']
+now = str(datetime.now())
+for character in string_to_replace: 
+    now = now.replace(character,'_') 
+
+audio = client.audio.speech.create(
+                model="tts-1",
+                voice="shimmer",
+                input=answer,
+                )
+
+with open(f"../backend/audio/{now}.mp3", 'wb') as f:
+    f.write(audio.content)
